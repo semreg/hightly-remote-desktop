c765be49537026f695d7cda599e59b9e72eca8b0
Semreg <vchabaniuk@yahoo.com>
2019-10-04 03:32:04 +0300

    Update project structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate env_logger;
 extern crate ws;
-
+extern crate web_view;
+extern crate webbrowser;
 extern crate enigo;
 
 #[macro_use]
@@ -14,8 +15,10 @@ mod input;
 
 use input::Input;
 
+use web_view::*;
+
 fn main() {
-    const PORT: i32 = 3012;
+    const PORT: i32 = 6061;
     const IP: &str = "0.0.0.0";
 
     env_logger::init();
@@ -23,22 +26,74 @@ fn main() {
     let addr = format!("{}:{}", IP, PORT);
 
     println!("\nListening on wss://{}\n", &addr);
-    
-    // Listen on an address and call the closure for each connection
-    if let Err(error) = ws::listen(addr, |out| {
-        // The handler needs to take ownership of out, so we use move
-        move |msg| {
-            println!("{}", msg);
-            
-            let msg_to_send: ws::Message = ws::Message::Text(String::from("OK"));
 
-            let new_input = Input::new(&msg).unwrap();
-            new_input.simulate();            
+    std::thread::spawn(move || {
+        // Listen on an address and call the closure for each connection
+        if let Err(error) = ws::listen(addr, |out| {
+            // The handler needs to take ownership of out, so we use move
+            move |msg| {
+                println!("{}", msg);
+                
+                let msg_to_send: ws::Message = ws::Message::Text(String::from("OK"));
 
-            out.send(msg_to_send)
+                let new_input = Input::new(&msg).unwrap();
+                new_input.simulate();            
+
+                out.send(msg_to_send)
+            }
+        }) {
+            // Inform the user of failure
+            println!("Failed to create WebSocket due to {:?}", error);
         }
-    }) {
-        // Inform the user of failure
-        println!("Failed to create WebSocket due to {:?}", error);
-    }
+    });
+
+    run_view(include_str!("../static/layouts/body.html"));
+}
+
+fn run_view (render_body: &str) {
+    let html = format!(r#"
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+            <style>{bootstrap}</style>
+            <style>{mdb}</style>
+            <style>{css}</style>
+        </head>
+        <body>
+            {body}
+        </body>
+        <script>{script}</script>
+        </html>
+    "#,
+    body = render_body,
+    css = include_str!("../static/styles/main.css"),
+    script = include_str!("../static/javascripts/main.js"),
+    mdb = include_str!("../static/bootstrap/mdb.min.css"),
+    bootstrap = include_str!("../static/bootstrap/bootstrap.min.css"));
+
+    WebViewBuilder::new()
+        .title("Higtlly Remote Desktop")
+        .content(Content::Html(html))
+        .size(420, 300)
+        .resizable(false)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|webview, arg| {
+            match arg {
+                "open" => {
+                    webbrowser::open("https://hightly.semreg.me/stream").unwrap();
+                },
+                "exit" => {
+                    webview.terminate();
+                }
+                _ => unimplemented!(),
+            };
+            Ok(())
+        })
+        .run()
+        .unwrap();
 }
